aff9ddf032d206872c8677d9678169c80ebf8931
tirthyakamaldasgupta-baartechnologies <tirthya.dasgupta@baar.ai>
2024-07-31 10:51:55 +0000

    chore: Included boiler plate code for split view.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,15 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { MonacoEditorModule } from 'ngx-monaco-editor-v2';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    importProvidersFrom(MonacoEditorModule.forRoot()),
+    provideClientHydration(),
+    provideRouter(routes),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+  ]
 };
--- src/app/pages/baar-data-visualizer/baar-data-visualizer.component.ts
+++ src/app/pages/baar-data-visualizer/baar-data-visualizer.component.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MonacoEditorModule } from 'ngx-monaco-editor-v2';
 
 @Component({
   selector: 'app-baar-data-visualizer',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    MonacoEditorModule
+  ],
   templateUrl: './baar-data-visualizer.component.html',
   styleUrl: './baar-data-visualizer.component.css'
 })
 export class BaarDataVisualizerComponent {
+  public monacoEditorOptions: any;
 
+  public data: string;
+
+  constructor(
+  ) {
+    this.data = '';
+
+    this._initializeMonacoEditorOptions();
+  }
+
+  private _initializeMonacoEditorOptions(): void {
+    this.monacoEditorOptions = {
+      fontFamily: 'Menlo, monospace',
+      fontSize: 14,
+      minimap: {
+        enabled: false
+      },
+      quickSuggestions: false,
+      language: 'plaintext',
+      // lineNumbers: 'off',
+      parameterHints: {
+        enabled: false
+      },
+      suggestOnTriggerCharacters: false,
+      theme: 'vs-light',
+      wordWrap: 'on'
+    };
+  }
 }
