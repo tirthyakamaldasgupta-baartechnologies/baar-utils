0741e04332b6628863c5c24cd0838a531837079c
tirthyakamaldasgupta-baartechnologies <tirthya.dasgupta@baar.ai>
2024-11-22 16:46:42 +0530

    feat: Add navbar component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NavbarComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
